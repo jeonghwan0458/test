966ac7d82a2a56044467afc0e0f56acf378da109
jeonghwan0458 <wjdghks0458@gmail.com>
2021-03-10 19:25:29 +0900

    test

---
+++ RF_READING/RF_READING.ino
@@ -0,0 +1,58 @@
+
+ #include <SPI.h> 
+
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // SPI 버스에 nRF24L01 라디오를 설정하기 위해 CE, CSN 선언.
+const byte address[6] = "00001"; //주소값을 5가지 문자열로 변경할 수 있으며, 송신기과 수신기가 동일한 주소로 해야됨.
+#include <Servo.h>
+Servo servo1;
+Servo servo2;
+int count1 = 0;
+int count2 = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(4,INPUT);
+  pinMode(9,INPUT);
+  servo1.attach(6);
+  servo2.attach(5);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN); //전원공급에 관한 파워레벨을 설정합니다. 모듈 사이가 가까우면 최소로 설정합니다.
+
+//거리가 가까운 순으로 RF24_PA_MIN / RF24_PA_LOW / RF24_PA_HIGH / RF24_PA_MAX 등으로 설정할 수 있습니다.
+
+//높은 레벨(거리가 먼 경우)은 작동하는 동안 안정적인 전압을 가지도록 GND와 3.3V에 바이패스 커패시터 사용을 권장함
+  radio.startListening(); //모듈을 수신기로 설정
+}
+void loop() {
+    int swich1 = digitalRead(9);
+    int swich2 = digitalRead(4);
+    Serial.println(swich2);
+  if (radio.available()) {
+    int text;
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+    
+    
+    if(text == 1 && count1 == 0 || swich1 == 1 ){
+        servo1.write(140);
+        count1 = 1;
+    }else if(text == 1 && count1 == 1){
+        servo1.write(80);
+        count1 = 0;
+    }
+
+    if(text == 0 && count2 == 0 || swich2 == 1){
+        servo2.write(140);
+        count2 = 1;
+    }else if(text == 0 && count2 == 1){
+        servo2.write(80);
+        count2 = 0;
+    }
+
+
+    
+  }
+}
